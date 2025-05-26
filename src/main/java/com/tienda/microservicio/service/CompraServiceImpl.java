b2ee3975b9754ef1772559a82f5fef82e4d42a30
Sebastián Olave Y. <165727846+walaJR@users.noreply.github.com>
2025-05-26 17:58:34 -0400

    Add files via upload

---
+++ src/main/java/com/tienda/microservicio/service/CompraServiceImpl.java
@@ -0,0 +1,57 @@
+package com.tienda.microservicio.service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tienda.microservicio.model.Boleta;
+import com.tienda.microservicio.model.DetalleBoleta;
+import com.tienda.microservicio.model.Producto;
+import com.tienda.microservicio.model.ProductoCompra;
+import com.tienda.microservicio.repository.BoletaRepository;
+import com.tienda.microservicio.repository.ProductoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CompraServiceImpl implements CompraService {
+
+    @Autowired
+    private ProductoRepository productoRepo;
+
+    @Autowired
+    private BoletaRepository boletaRepo;
+
+    @Override
+    public Boleta procesarCompra(List<ProductoCompra> carrito) {
+
+        Boleta boleta = new Boleta();
+        boleta.setFecha(LocalDateTime.now());
+        List<DetalleBoleta> detalles = new ArrayList<>();
+
+        double total = 0;
+        for (ProductoCompra item : carrito) {
+            Producto producto = productoRepo.findById(item.getIdProducto())
+                    .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+
+            DetalleBoleta detalle = new DetalleBoleta();
+            detalle.setProducto(producto);
+            detalle.setCantidad(item.getCantidad());
+            detalle.setPrecioUnitario(producto.getPrecio());
+            detalle.setBoleta(boleta);
+
+            detalles.add(detalle);
+            total += producto.getPrecio() * item.getCantidad();
+        }
+
+        boleta.setDetalles(detalles);
+        boleta.setTotal(total);
+
+        return boletaRepo.save(boleta);
+    }
+
+}
